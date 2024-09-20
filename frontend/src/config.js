4910a44012793569d1c806c9b56db8c23caf27bc
yash sheliya <135209192+yashsheliya@users.noreply.github.com>
2024-09-20 15:13:44 +0530

    first commit

---
+++ frontend/src/config.js
@@ -0,0 +1,2 @@
+// src/config.js
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:8080/products";
+            const url = `mern-stack-app-login-signup-frontend.vercel.app/products`;
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
